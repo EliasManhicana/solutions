92cbacde51d8e0fd7648a6b5d71e4ec97a65c935
Elias Manhicana <eliasmanhicane4@gmail.com>
2022-04-24 20:19:07 +0200

    Redimensionamento da animação de texto

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Sites", "Sistemas Web", "Sistemas Java", "Lojas Virtuais", "Lojas para Catálogo"],
+        strings: ["Sites", "Sistemas Web", "Sistemas Java", "Lojas Virtuais", "Lojas Catálogo"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Sites", "Sistemas Web", "Sistemas Java", "Lojas Virtuais", "Lojas para Catálogo"],
+        strings: ["Sites", "Sistemas Web", "Sistemas Java", "Lojas Virtuais", "Lojas Catálogo"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
